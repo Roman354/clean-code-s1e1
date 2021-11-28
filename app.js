53f75abd27b15be2b7d3d7695141743bfd2e0e51
Roman354 <RomanBianko14@gmail.com>
2021-11-29 01:32:02 +0300

    fix: alternative for multimedia (advanced level) to rule 1.2 in index.html, app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
     deleteButtonImg.classList.add("dltImg");
-
+    deleteButtonImg.alt = "delete arrow-button";
     label.innerText=taskString;
     label.className='task';
 
